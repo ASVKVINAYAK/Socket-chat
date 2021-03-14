a897172ef753a94d7a43cd5dbfef3045c52a0f2f
Zeo-shark <sauravberaiit@gmail.com>
2021-03-14 15:57:12 +0530

    remove all previous version folders

--- README.md
+++
@@ -1,2 +0,0 @@
-# Elan-App-Dev
-Project built for Elan and nVision Hackathon by IIT Hyderabad Event Name: APP DEV
--- chatapp/android/app/src/main/kotlin/com/asvkvinayak/chatapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.asvkvinayak.chatapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
